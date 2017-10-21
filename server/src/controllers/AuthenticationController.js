06de850dd38d9342305586b6863bd128b8a2b2d7
rojimark <rmbedana@Roggies-MacBook-Pro.local>
2017-10-22 00:46:49 +0800

    added registration validation

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,7 +1,15 @@
+const {User} = require('../models')
+
 module.exports = {
-	register (req, res) {
-		res.send({
-			message: `Hello ${req.body.email}! Your user was registered!`
-		})
+
+	async register (req, res) {
+		try {
+			const user = await User.create(req.body)
+			res.send(user.toJSON())
+		} catch (err){
+			res.status(400).send({
+				error:'This account already exist.'
+			})
+		}
 	}
 }
