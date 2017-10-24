66ecca8104c5056fe7383686ae26b75b3583d29e
rojimark <rmbedana@Roggies-MacBook-Pro.local>
2017-10-24 10:24:20 +0800

    added login form

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -5,7 +5,7 @@
         <a href="#" class="left brand-logo" ><router-link to="/">TabTracker</router-link></a>
         <ul id="nav-mobile" class="right hide-on-s-and-down">
           <li><a href="#">Browse</a></li>
-          <li><a href="#">Menu 2</a></li>
+          <li><a href="#" <router-link to="login">Login</router-link>Login</a></li>
           <li><a href="#" <router-link to="register">Register</router-link>Sign Up</a></li>
         </ul>
       </div>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,58 @@
+<template>
+
+
+      <div class="row">
+        <div class="col s12 m6 offset-m3">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">Login</span>
+              <input type="email" name="email" v-model="email" placeholder="email" />
+
+              <input type="password" name="password" v-model="password" placeholder="password" />
+              <div class="error" v-html="error"></div>
+              <button  class="teal btn" @click="register">Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        await AuthenticationService.login({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error{
+  color:#FF0000;
+}
+.row{
+  margin-top: 30px;
+}
+</style>
