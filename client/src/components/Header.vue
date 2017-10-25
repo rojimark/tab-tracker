dd439177e49f916427369954820d39cf4e30a2c7
rojimark <rmbedana@Roggies-MacBook-Pro.local>
2017-10-26 07:28:10 +0800

    added Songs page

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -3,9 +3,11 @@
     <nav>
       <div class="nav-wrapper blue-grey darken-1">
         <a href="#" class="left brand-logo" ><router-link to="/">TabTracker</router-link></a>
-        <ul id="nav-mobile" class="right hide-on-s-and-down" v-if="!$store.state.isUserLoggedIn">
-          <li><a href="#" <router-link to="login">Login</router-link>Login</a></li>
-          <li><a href="#" <router-link to="register">Register</router-link>Sign Up</a></li>
+        <ul id="nav-mobile" class="right hide-on-s-and-down">
+          <li class="left"><a href="#" <router-link to="songs">Browse</router-link>Browse</a></li>
+          <li v-if="!$store.state.isUserLoggedIn"><a href="#" <router-link to="login">Login</router-link>Login</a></li>
+          <li v-if="!$store.state.isUserLoggedIn"><a href="#" <router-link to="register">Register</router-link>Sign Up</a></li>
+          <li v-if="$store.state.isUserLoggedIn"><a href="#" @click="logout" <router-link to="/">Logout</router-link>Sign Up</a></li>
         </ul>
       </div>
     </nav>
@@ -14,7 +16,12 @@
 
 <script>
   export default {
-
+    methods: {
+      logout () {
+        this.$store.dispatch('setToken', null)
+        this.$store.dispatch('setUser', null)
+      }
+    }
   }
 </script>
 
