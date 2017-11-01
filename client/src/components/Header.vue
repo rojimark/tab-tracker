8a85f3e872f14573df33c53083c2cbcbe9d46001
rojimark <rmbedana@Roggies-MacBook-Pro.local>
2017-11-01 11:35:42 +0800

    redirected home page to songs page

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -7,9 +7,10 @@
         <a href="#" class="header-logo brand-logo" ><router-link to="/">TabTracker</router-link></a>
         <ul id="nav-mobile" class="right hide-on-s-and-down">
           <li class="left"><a href="#" <router-link to="/songs">Browse</router-link></a></li>
-          <li v-if="!$store.state.isUserLoggedIn"><a href="#" <router-link to="/login">Login</router-link></a></li>
-          <li v-if="!$store.state.isUserLoggedIn"><a href="#" <router-link to="/register">Register</router-link></a></li>
-          <li v-if="$store.state.isUserLoggedIn"><a href="#" v-on:click="logout"><router-link to="/">Logout</router-link></a></li>
+          <li v-if="!$store.state.isUserLoggedIn"><a href="#" <router-link :to="{ name: 'login'}">Login</router-link></a></li>
+          <li v-if="!$store.state.isUserLoggedIn"><a href="#" <router-link :to="{ name: 'register'}">Register</router-link></a></li>
+          <li v-if="$store.state.isUserLoggedIn"><a href="#" v-on:click="logout"><router-link :to="{ name: 'songs' }">Logout</router-link></a></li>
+          <!-- <router-link :to="{ name: '', params: {} }"></router-link> -->
         </ul>
       </div>
     </nav>
